f5c01ef423c8d99f94f3a1e88b81830705f8c8b2
Antonio Ono <me@antonioono.com>
2014-09-21 23:58:32 -0400

    Changing bg

--- static/js/intro.js
+++ static/js/intro.js
@@ -5,21 +5,24 @@ $(document).ready(function(){
                     "donec id elit non mi",
                     "porta gravida at eget metus",
                     "duis mollis"],
+        images  =  ["http://placekitten.com/2048",
+                    "http://placekitten.com/2049",
+                    "http://placekitten.com/2050",
+                    "http://placekitten.com/2051",
+                    "http://placekitten.com/2052",
+                    "http://placekitten.com/2053"],
         span    = $("body > header h1 span"),
         i       = 0;
 
     setInterval(function(){
         var title = events[i];
-        incrementEvent(title);
-        i++;
         
+        span.text(title);
+        $("body").css("background-image", "url('" + images[i] + "')");
+
+        i++;
         if (i >= events.length) {
             i = 1;
         }
     }, 250);
-
-    function incrementEvent(title) {
-        $(span).text(title);
-        console.log(span);
-    }
 });
