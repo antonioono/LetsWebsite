cde5fcbf7f9babda7a5d5161b4e668f3b3417d54
Antonio Ono <me@antonioono.com>
2014-12-19 15:21:25 -0500

    animated gradient

--- static/js/intro.js
+++ static/js/intro.js
@@ -14,35 +14,72 @@ $(document).ready(function(){
                     "check out tropical tuesday at Brillbox",
                     "get belgian waffle"]
         span    = $("body > header h1 span"),
-        oldEvent = {text : 0, image : 0},
-        newEvent = {text : 0, image : 0};
-        
+        oldEvent = 0,
+        newEvent = 0,
+        bgTurn   = 0;        
+
     setInterval(function(){        
-        span.text(events[newRandom("text")]);
-        $("body").css("background", "-webkit-linear-gradient("+gradient("top")+" 0%,"+gradient("bottom")+" 100%) fixed");
-    }, 250);
-    
-    function gradient(side) {
-        if (side == "top") {
-            return "orange";
+        span.text(events[newRandom()]);
+    }, 125);
+
+    /*
+setInterval(function(){
+        $(".bg").css("opacity", bgTurn);
+        var gradient = makeGradient();
+        if (bgTurn == 0) {
+            $(".bg .inner").css("background-image", "-webkit-linear-gradient("+gradient[0]+" 0%,"+gradient[1]+" 100%)");
+            bgTurn = 1;
         } else {
-            return "red";
+            $("body").css("background-image", "-webkit-linear-gradient("+gradient[0]+" 0%,"+gradient[1]+" 100%)");
+            bgTurn = 0;
         }
-    }
+    }, 1000);
+*/
     
-    function newRandom(kind) {
-        newEvent[kind] = Math.floor(Math.random() * events.length);
-        compare(newEvent[kind], oldEvent[kind], kind);
-        oldEvent[kind] = newEvent[kind];
-        return newEvent[kind];
-    }
+    setInterval(function(){
+        $(".bg").css("opacity", bgTurn);
+    }, 62.5);
     
-    function compare(a, b, kind) {
-        if (a == b) {
-            newEvent[kind] = Math.floor(Math.random() * events.length);
-            compare(newEvent[kind], oldEvent[kind], kind);
+    setInterval(function(){
+        var gradient = makeGradient();
+        if (bgTurn == 0) {
+            $(".bg .inner").css("background-image", "-webkit-linear-gradient("+gradient[0]+" 0%,"+gradient[1]+" 100%)");
+            bgTurn = 1;
         } else {
-            return;
+            $("body").css("background-image", "-webkit-linear-gradient("+gradient[0]+" 0%,"+gradient[1]+" 100%)");
+            bgTurn = 0;
+        }
+    }, 125)
+    
+    function makeGradient() {
+        var color,
+            colorValues     = [0,0,0],
+            primaryRgb      = Math.floor(Math.random()*3),
+            secondaryRgb    = deDupe(Math.floor(Math.random()*3), primaryRgb);
+            
+        colorValues[primaryRgb]   = 255;
+        colorValues[secondaryRgb] = Math.floor(Math.random()*255);
+        color = "rgb("+colorValues[0]+","+colorValues[1]+","+colorValues[2]+")";
+        var secondColorValues = colorValues;
+//         secondColorValues[secondaryRgb] = Math.max((colorValues[secondaryRgb] - 100), 0);
+//         secondColorValues[secondaryRgb] = Math.floor(Math.random()*255);
+        secondColorValues[secondaryRgb] = 255;
+        secondColorValues[primaryRgb] = Math.floor(Math.random()*(255 - 128) + 128);
+        var secondColor = "rgb("+secondColorValues[0]+","+secondColorValues[1]+","+secondColorValues[2]+")";
+        return [color, secondColor];
+    }
+    
+    function newRandom() {
+        newEvent = deDupe(Math.floor(Math.random() * events.length), oldEvent);
+        oldEvent = newEvent;
+        return newEvent;
+    }
+    
+    function deDupe(a, b) {
+        if (a === b) {
+            return (b == 0) ? a + 1 : a - 1;
+        } else {
+            return a;
         }
     }
 }); 
